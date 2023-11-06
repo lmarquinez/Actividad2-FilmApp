cd4fbfabd00e87bc2a0bee78115ef52117a01a7e
Lauri <rapiri4@hotmail.com>
2023-11-06 21:55:55 +0100

    First commit with the main structure
    - Added bootstrap & Angular Material

---
+++ FilmApp-Angular/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
+import { CardComponent } from './components/card/card.component';
+import { FilmListComponent } from './components/film-list/film-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    FormComponent,
+    CardComponent,
+    FilmListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FilmApp-Angular/src/app/components/film-card/film-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-card',
+  templateUrl: './film-card.component.html',
+  styleUrls: ['./film-card.component.css']
+})
+export class FilmCardComponent {
+
+}
