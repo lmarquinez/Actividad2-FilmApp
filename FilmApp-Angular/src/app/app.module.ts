0efd191c5a6c819454bc4595d09085b16db66629
Lauri <rapiri4@hotmail.com>
2023-11-06 23:19:06 +0100

    Added footer and header

--- FilmApp-Angular/src/app/app.module.ts
+++ FilmApp-Angular/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
-import { CardComponent } from './components/card/card.component';
+import { FilmCardComponent } from './components/film-card/film-card.component';
 import { FilmListComponent } from './components/film-list/film-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -16,13 +16,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FooterComponent,
     HeaderComponent,
     FormComponent,
-    CardComponent,
-    FilmListComponent
+    FilmCardComponent,
+    FilmListComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
